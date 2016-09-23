d5f2c3d6e28df6c555d190ad52da9a9c464ccd7b
Arturo Cordero <vakmancordero@gmail.com>
2016-09-22 23:28:25 -0500

    Commit 2 xDDD

--- src/biometricauthentication/BiometricAuthentication.java
+++ src/biometricauthentication/BiometricAuthentication.java
@@ -1,21 +1,11 @@
 package biometricauthentication;
 
-
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Iterator;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.application.Application;
-import javafx.stage.FileChooser;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -25,7 +15,7 @@ public class BiometricAuthentication extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        /*
+        
         Parent root = FXMLLoader.load(getClass().getResource("BiometricFXML.fxml"));
         
         Scene scene = new Scene(root);
@@ -37,34 +27,6 @@ public class BiometricAuthentication extends Application {
         stage.setTitle("Biometric Authentication");
         stage.setScene(scene);
         stage.show();
-        */
-        
-        stage.show();
-        
-        File myFile = new FileChooser().showOpenDialog(null); 
-        FileInputStream fis = new FileInputStream(myFile); 
-        
-        XSSFWorkbook myWorkBook = new XSSFWorkbook (fis); 
-        
-        XSSFSheet mySheet = myWorkBook.getSheetAt(0); 
-        
-        Iterator<Row> rowIterator = mySheet.iterator();
-        
-        while (rowIterator.hasNext()) {
-            
-            Row row = rowIterator.next();
-            
-            Iterator<Cell> cellIterator = row.cellIterator();
-            
-            while (cellIterator.hasNext()) {
-                
-                Cell cell = cellIterator.next();
-                
-                System.out.println(cell.getStringCellValue());
-                
-            }
-            
-        }
         
     }
     
